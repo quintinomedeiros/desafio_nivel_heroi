8f49736a29e53a8defdd875de81722580b4850ca
quintinomedeiros <quintinomedeiros@yaho.com.br>
2024-10-25 17:30:42 -0300

    nivel_heroi_final

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ let nomeHeroi = prompt("Qual é o nome do heroi?");
 let experienciaHeroi = parseInt(prompt("Qual é a experiência do heroi? "));
 
 if (experienciaHeroi <= 1000) {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Ferro!`);
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Ferro!`);
 } else if (experienciaHeroi > 1000 && experienciaHeroi <= 2000) {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Bronze!`);
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Bronze!`);
 } else if (experienciaHeroi > 2000 && experienciaHeroi <= 5000) {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Prata Ouro!`);
-} else if (experienciaHeroi > 5000 && experienciaHeroi <= 8000) {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Platina Diamante!`);
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Prata!`);
+} else if (experienciaHeroi > 5000 && experienciaHeroi <= 7000) {
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Ouro!`);
+} else if (experienciaHeroi > 7000 && experienciaHeroi <= 8000) {
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Platina!`);
 } else if (experienciaHeroi > 8000 && experienciaHeroi <= 9000) {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Ascendente!`);
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Ascendente!`);
 } else if (experienciaHeroi > 9000 && experienciaHeroi <= 10000) {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Imortal!`);
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Imortal!`);
 } else {
-    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e é nível Radiante!`);
+    console.log(`O heroi ${nomeHeroi} tem xp de ${experienciaHeroi} e está no nível Radiante!`);
 }
